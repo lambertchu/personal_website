dabb84821ff8c22f0a9dd845caecd8e824b5088a
Lambert Chu <lambert.chu@gmail.com>
2021-10-31 11:57:46 -0400

    Remove unneeded content and pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,13 +51,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `projects`,
-        path: `${__dirname}/content/projects`,
-      },
-    },
-    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
